fad1692529731cfc204efdeec175cebd03f1d262
sebastian vasquez <seba.vasquez.m@gmail.com>
2019-10-16 18:15:33 -0300

    Add http2 server response with h2c prior knowledge

---
+++ app/src/main/java/cl/svasquezm/android_http2_protobuf/MainActivity.kt
@@ -0,0 +1,55 @@
+package cl.svasquezm.android_http2_protobuf
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import okhttp3.OkHttpClient
+import okhttp3.Protocol
+import okhttp3.Request
+
+class MainActivity : AppCompatActivity() {
+    val TAG = "AH2"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        request()
+    }
+
+    fun request(){
+
+        GlobalScope.run {
+            launch {
+                val protocolList = mutableListOf<Protocol>()
+                protocolList.add(Protocol.H2_PRIOR_KNOWLEDGE)
+
+                val client = OkHttpClient.Builder().apply {
+                    protocols(protocolList)
+                }.build()
+
+                val request = Request.Builder()
+                    .url("http://127.0.0.1")
+                    .build()
+
+                val response = client.newCall(request).execute()
+                Log.i(TAG, "Server response: ")
+                response.headers.forEach {
+                    Log.i(TAG, " ${it.first}: ${it.second} ")
+                }
+
+                Log.i(TAG, "Protocol: ${response.protocol.name} ${response.protocol.ordinal}")
+
+
+                val user = Models.User.parseFrom(response.body!!.bytes())
+                Log.i(TAG, "User.name = ${user.name}")
+                Log.i(TAG, "User.email = ${user.email}")
+                Log.i(TAG, "User.type = ${user.type}")
+                user.petsList.forEachIndexed { i, pet ->
+                    Log.i(TAG, "User.pet$i = (${pet.type}, name: ${pet.name})")
+                }
+            }
+        }
+    }
+}
---
+++ server/src/main/java/cl/svasquezm/server/ProtobufHTTP2Server.kt
@@ -0,0 +1,59 @@
+package cl.svasquezm.server
+
+import cl.svasquezm.proto.Models
+import io.javalin.Javalin
+import org.eclipse.jetty.http2.server.HTTP2CServerConnectionFactory
+import org.eclipse.jetty.server.HttpConfiguration
+import org.eclipse.jetty.server.HttpConnectionFactory
+import org.eclipse.jetty.server.Server
+import org.eclipse.jetty.server.ServerConnector
+import java.io.ByteArrayInputStream
+
+class ProtobufHTTP2Server {
+    companion object {
+
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val app = Javalin.create {
+                it.server { createServer() }
+            }.start()
+
+            val model = Models.User.newBuilder().apply {
+                name = "Sebastian"
+                email = "seba.vasquez.m@gmail.com"
+                type = Models.User.Type.MOBILE
+                addPets(Models.Pet.newBuilder().apply {
+                    type = "dog"
+                    name = "luka"
+                })
+                addPets(Models.Pet.newBuilder().apply {
+                    type = "cat"
+                    name = "ema"
+                })
+                addPets(Models.Pet.newBuilder().apply {
+                    type = "rabbit"
+                    name = "gaspar"
+                })
+            }.build()
+
+            app.get("/") {
+                it.contentType("application/protobuf")
+                it.result(ByteArrayInputStream(model.toByteArray()))
+            }
+        }
+
+        fun createServer(): Server {
+            val httpsConfig = HttpConfiguration()
+            val http1 = HttpConnectionFactory(httpsConfig)
+            val http2 = HTTP2CServerConnectionFactory(httpsConfig)
+
+            return Server().apply {
+
+                //HTTP/2 Connector
+                addConnector(ServerConnector(server, http2).apply {
+                    port = 80
+                })
+            }
+        }
+    }
+}
